5459c181692800b66d09b67d6fdf95a80ef990c6
raymondngiam <ha.ngiam87@gmail.com>
2022-03-12 16:55:09 +0800

    Use explicit namespace for readability

--- main.cpp
+++ main.cpp
@@ -9,22 +9,9 @@
 #include <sophus/se3.hpp>
 #include <ceres/ceres.h>
 
-using Eigen::MatrixXd;
-using Eigen::Vector3d;
-using Eigen::AngleAxisd;
-using Eigen::Quaterniond;
-using Sophus::SE3d;
-
-using ceres::AutoDiffCostFunction;
-using ceres::LocalParameterization;
-using ceres::CostFunction;
-using ceres::Problem;
-using ceres::Solve;
-using ceres::Solver;
-
 class CustomCeresCallback : public ceres::IterationCallback {
     public:
-        CustomCeresCallback(const Problem& _problem):
+        CustomCeresCallback(const ceres::Problem& _problem):
             problem(_problem) {
             logger.open("../data/log.csv", std::ios_base::out);
             logger << "iteration, loss, angle, ax, ay, az, tx, ty, tz\n";
@@ -38,9 +25,9 @@ class CustomCeresCallback : public ceres::IterationCallback {
             for(const auto& block : paramBlocks){
                 auto size = problem.ParameterBlockSize(block);
                 
-                Eigen::Map<SE3d const> const pose(block);
-                Quaterniond so3(pose.so3().data());
-                AngleAxisd aa(so3);
+                Eigen::Map<Sophus::SE3d const> const pose(block);
+                Eigen::Quaterniond so3(pose.so3().data());
+                Eigen::AngleAxisd aa(so3);
 
                 logger << summary.iteration << ", "
                         << summary.cost << ", "
@@ -55,11 +42,11 @@ class CustomCeresCallback : public ceres::IterationCallback {
             return ceres::SOLVER_CONTINUE;
         }
     private:
-        const Problem& problem;
+        const ceres::Problem& problem;
         std::ofstream logger;
 };
 
-class LocalParameterizationSE3 : public LocalParameterization {
+class LocalParameterizationSE3 : public ceres::LocalParameterization {
     public:
         virtual ~LocalParameterizationSE3() {}
 
@@ -69,10 +56,10 @@ class LocalParameterizationSE3 : public LocalParameterization {
         //
         virtual bool Plus(double const* T_raw, double const* delta_raw,
                           double* T_plus_delta_raw) const {
-            Eigen::Map<SE3d const> const T(T_raw);
+            Eigen::Map<Sophus::SE3d const> const T(T_raw);
             Eigen::Map<Sophus::Vector6d const> const delta(delta_raw);
-            Eigen::Map<SE3d> T_plus_delta(T_plus_delta_raw);
-            T_plus_delta = T * SE3d::exp(delta);
+            Eigen::Map<Sophus::SE3d> T_plus_delta(T_plus_delta_raw);
+            T_plus_delta = T * Sophus::SE3d::exp(delta);
             return true;
         }
 
@@ -82,23 +69,23 @@ class LocalParameterizationSE3 : public LocalParameterization {
         //
         virtual bool ComputeJacobian(double const* T_raw,
                                      double* jacobian_raw) const {
-            Eigen::Map<SE3d const> T(T_raw);
-            Eigen::Map<Eigen::Matrix<double, 
-                                     SE3d::num_parameters, 
-                                     SE3d::DoF, 
+            Eigen::Map<Sophus::SE3d const> T(T_raw);
+            Eigen::Map<Eigen::Matrix<double,
+                                     Sophus::SE3d::num_parameters,
+                                     Sophus::SE3d::DoF,
                                      Eigen::RowMajor>>jacobian(jacobian_raw);
             jacobian = T.Dx_this_mul_exp_x_at_0();
             return true;
         }
 
-        virtual int GlobalSize() const { return SE3d::num_parameters; }
+        virtual int GlobalSize() const { return Sophus::SE3d::num_parameters; }
 
-        virtual int LocalSize() const { return SE3d::DoF; }
+        virtual int LocalSize() const { return Sophus::SE3d::DoF; }
 };
 
 struct CostFunctor {
-    CostFunctor(const Vector3d& target,
-                const Vector3d& input):
+    CostFunctor(const Eigen::Vector3d& target,
+                const Eigen::Vector3d& input):
         input(input), target(target){}
 
     template <typename T>
@@ -109,8 +96,8 @@ struct CostFunctor {
         return true;
     }
 
-    Vector3d input;
-    Vector3d target;
+    Eigen::Vector3d input;
+    Eigen::Vector3d target;
 };
 
 enum ICP_SOLVER_TYPE{
@@ -146,15 +133,15 @@ int main(int argc, char** argv){
     }
     fmt::print("solverType: {0}\n", ToString(solverType));
 
-    Vector3d axis_groundtruth_inv;
+    Eigen::Vector3d axis_groundtruth_inv;
     axis_groundtruth_inv << 0,0,1;
-    AngleAxisd aa_groundtruth_inv(-M_PI/3.0,axis_groundtruth_inv.normalized());
-    Quaterniond q_groundtruth_inv(aa_groundtruth_inv);
-        
-    Vector3d t_groundtruth_inv;
-    t_groundtruth_inv << -0.300,0.100,0.;    
-    
-    SE3d T_tmp(q_groundtruth_inv,t_groundtruth_inv);
+    Eigen::AngleAxisd aa_groundtruth_inv(-M_PI/3.0,axis_groundtruth_inv.normalized());
+    Eigen::Quaterniond q_groundtruth_inv(aa_groundtruth_inv);
+
+    Eigen::Vector3d t_groundtruth_inv;
+    t_groundtruth_inv << -0.300,0.100,0.;
+
+    Sophus::SE3d T_tmp(q_groundtruth_inv,t_groundtruth_inv);
     auto T_groundtruth = T_tmp.inverse();
     std::cout<<"SE3 in HTM:"<<std::endl;
     std::cout<<T_groundtruth.matrix()<<std::endl;
@@ -164,8 +151,8 @@ int main(int argc, char** argv){
     double ax_groundtruth,ay_groundtruth,az_groundtruth;  
     double tx_groundtruth,ty_groundtruth,tz_groundtruth;
 
-    Quaterniond q_groundtruth(T_groundtruth.so3().data());
-    AngleAxisd  aa_groundtruth(q_groundtruth);
+    Eigen::Quaterniond q_groundtruth(T_groundtruth.so3().data());
+    Eigen::AngleAxisd  aa_groundtruth(q_groundtruth);
     angle_groundtruth = aa_groundtruth.angle();
     ax_groundtruth=aa_groundtruth.axis()[0];
     ay_groundtruth=aa_groundtruth.axis()[1];;
@@ -198,7 +185,7 @@ int main(int argc, char** argv){
     }
 
     int N = line_count;
-    MatrixXd points_(N,3);
+    Eigen::MatrixXd points_(N,3);
     std::ifstream file(dataFile);
     std::string line;
     int currentIdx = 0;
@@ -216,22 +203,22 @@ int main(int argc, char** argv){
     std::cout<<N<<" points loaded:"<<std::endl;
     auto targetPoints = points_.transpose();
 
-    MatrixXd sourcePoints(3,N);
+    Eigen::MatrixXd sourcePoints(3,N);
     for(uint32_t i=0; i<N; i++){
-        Vector3d p = targetPoints.col(i);
+        Eigen::Vector3d p = targetPoints.col(i);
         auto transP = T_tmp*p;
         sourcePoints.col(i)=transP;
     }
     
     double angle(0);
-    Vector3d axis;
+    Eigen::Vector3d axis;
     axis << 0,0,0;
-    AngleAxisd aa(angle,axis.normalized());
-    Quaterniond q(aa);
-    
-    Vector3d t;
+    Eigen::AngleAxisd aa(angle,axis.normalized());
+    Eigen::Quaterniond q(aa);
+
+    Eigen::Vector3d t;
     t << 0,0,0;
-    SE3d T(q,t);
+    Sophus::SE3d T(q,t);
     
     double angle_init = angle;
     double ax_init = axis[0];
@@ -243,21 +230,21 @@ int main(int argc, char** argv){
     
     ceres::Problem problem;
     problem.AddParameterBlock(T.data(),
-                                SE3d::num_parameters,
+                              Sophus::SE3d::num_parameters,
                                 new LocalParameterizationSE3);
     for(uint32_t i=0; i<N; i++){
-        Vector3d target = targetPoints.col(i);
-        Vector3d input = sourcePoints.col(i);
-        CostFunction* cost_function =
-            new AutoDiffCostFunction<CostFunctor, 
+        Eigen::Vector3d target = targetPoints.col(i);
+        Eigen::Vector3d input = sourcePoints.col(i);
+        ceres::CostFunction* cost_function =
+            new ceres::AutoDiffCostFunction<CostFunctor,
                                      3,                     // number of residual
-                                     SE3d::num_parameters   // number of parameter for first parameter block 
+                                     Sophus::SE3d::num_parameters   // number of parameter for first parameter block
                                     >(new CostFunctor(target, input));
         problem.AddResidualBlock(cost_function, NULL,T.data());
     }
     
     // Run the solver!
-    Solver::Options options;
+    ceres::Solver::Options options;
     options.max_num_iterations = 50;
     options.parameter_tolerance = 1e-6;
 
@@ -292,13 +279,13 @@ int main(int argc, char** argv){
     CustomCeresCallback myCallback(problem);
     options.callbacks.push_back(&myCallback);
     options.minimizer_progress_to_stdout = true;
-    Solver::Summary summary;
-    Solve(options, &problem, &summary);
+    ceres::Solver::Summary summary;
+    ceres::Solve(options, &problem, &summary);
     std::cout << summary.FullReport() << std::endl;
     std::cout << std::endl;
     
-    Quaterniond q_out(T.so3().data());
-    AngleAxisd aa_out(q_out);
+    Eigen::Quaterniond q_out(T.so3().data());
+    Eigen::AngleAxisd aa_out(q_out);
     auto ax_optimized = aa_out.axis()[0];
     auto ay_optimized = aa_out.axis()[1];
     auto az_optimized = aa_out.axis()[2];
